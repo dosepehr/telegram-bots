57f374b8d0a3d3c605e805df9b0ef9d21460ebc4
dosepehr <dosepehr@gmail.com>
2025-02-07 15:18:25 +0330

    add sending audio

--- translation-bot/index.js
+++ translation-bot/index.js
@@ -1,29 +1,33 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = '7984012494:AAGa9jcfdpuxCzcHbcgB6EHw_FdDXVPw5eQ';
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
+bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
+    console.log(chatId);
+    const inlineKeyboard = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: 'دریافت عکس 🖼️', callback_data: '/photo' },
+                    { text: 'دریافت آهنگ 🎵', callback_data: '/audio' },
+                ],
+                [{ text: '🫡 دریافت استیکر', callback_data: '/sticker' }],
+            ],
+        },
+    };
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
+    bot.sendMessage(chatId, 'start command', inlineKeyboard);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+bot.on('callback_query', (query) => {
+    const chatId = query.from.id;
+    if (query.data == '/audio') {
+        bot.sendAudio(chatId, 'assets/audio.mp3', {
+            title: 'ای یار من',
+            caption: 'آهنگ جدید شجریان',
+        });
+    }
 });

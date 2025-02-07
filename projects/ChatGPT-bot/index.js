585e23a2b535d94244632d11d04b9d264915b080
dosepehr <dosepehr@gmail.com>
2025-02-08 01:03:22 +0330

    add ending & continuing chat

--- projects/ChatGPT-bot/index.js
+++ projects/ChatGPT-bot/index.js
@@ -2,6 +2,7 @@ import { Markup, Telegraf } from 'telegraf';
 import redis from '../../utils/redis.js';
 import { sendRequest } from './utils/sendRequest.js';
 const bot = new Telegraf('7866634027:AAH2UNM9ka1_A4BK5bfnIWfcA5rUIcyMjFU');
+
 bot.start((ctx) => {
     ctx.reply(
         'به ربات ChatGPT خوش اومدید!\n لطفا بگو میخوای از کدوم سرویس استفاده کنی',
@@ -17,11 +18,22 @@ bot.action('turbo', (ctx) => {
     redis.set(`user:${chatId}:engine`, 'gpt3.5-turbo', { EX: 180 });
     ctx.editMessageText('سلام!\n چه کمکی میتونم به شما بکنم؟');
 });
+
 bot.action('GPT4', (ctx) => {
     const chatId = ctx.chat.id;
     redis.set(`user:${chatId}:engine`, 'gpt4o', { EX: 180 });
     ctx.editMessageText('سلام!\n چه کمکی میتونم به شما بکنم؟');
 });
+
+bot.action('end', (ctx) => {
+    const chatId = ctx.chat.id;
+    ctx.editMessageText('مکالمه به پایان رسید⛔');
+    redis.del(`user:${chatId}:engine`);
+});
+bot.action('continue', (ctx) => {
+    ctx.editMessageText('دیگه چه کمکی میتونم بهت بکنم؟');
+});
+
 bot.on('text', async (ctx) => {
     const chatId = ctx.chat.id;
     const text = ctx.text;
@@ -29,13 +41,30 @@ bot.on('text', async (ctx) => {
     const engine = await redis.get(`user:${chatId}:engine`);
     if (engine) {
         const data = await sendRequest({ content: text, engine });
+        ctx.reply('درخواست شما در حال پردازش است');
         if (data.status == 200) {
             ctx.reply(data.result);
+            ctx.reply(
+                'درخواست شما با موفقیت پردازش شد',
+                Markup.inlineKeyboard([
+                    Markup.button.callback('اتمام گفتگو', 'end'),
+                    Markup.button.callback('ادامه گفتگو', 'continue'),
+                ])
+            );
         } else {
             ctx.reply('مشکلی پیش آمده');
         }
+    } else {
+        ctx.reply(
+            'لطفا ابتدا موتور مورد نیاز خود را مشخص کنید',
+            Markup.inlineKeyboard([
+                Markup.button.callback('ChatGPT 3.5 Turbo', 'turbo'),
+                Markup.button.callback('ChatGPT 4', 'GPT4'),
+            ])
+        );
     }
 });
+
 bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));

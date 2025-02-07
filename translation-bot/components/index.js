147d46c1d2855c442a7b2b270b9c5a3af07364ac
dosepehr <dosepehr@gmail.com>
2025-02-07 17:37:05 +0330

    save user actions in redis

--- translation-bot/components/index.js
+++ translation-bot/components/index.js
@@ -2,9 +2,26 @@ export const homeMenu = {
     reply_markup: {
         inline_keyboard: [
             [
-                { text: 'ترجمه با گوگل', callback_data: '/google' },
-                { text: 'ترجمه با مایکروسافت', callback_data: '/microsoft' },
-                { text: 'ترجمه با یندکس', callback_data: '/yandex' },
+                { text: 'ترجمه با گوگل', callback_data: 'google' },
+                { text: 'ترجمه با مایکروسافت', callback_data: 'microsoft' },
+                { text: 'ترجمه با یندکس', callback_data: 'yandex' },
+            ],
+        ],
+    },
+};
+
+export const sourseLangMenu = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: 'فارسی 🦁☀️', callback_data: 'fa' },
+                { text: '🇺🇸 انگلیسی', callback_data: 'en' },
+                { text: '🇸🇦 عربی', callback_data: 'ar' },
+            ],
+            [
+                { text: '🇩🇪 آلمانی', callback_data: 'de' },
+                { text: '🇮🇹 ایتالیایی', callback_data: 'it' },
+                { text: '🇫🇷 فرانسوی', callback_data: 'fr' },
             ],
         ],
     },
--- translation-bot/index.js
+++ translation-bot/index.js
@@ -1,20 +1,36 @@
 import TelegramBot from 'node-telegram-bot-api';
 import mysql from '../utils/mysql.js';
-import redis from '../utils/redis.js'
-import { homeMenu } from './components/index.js';
+import redis from '../utils/redis.js';
+import { homeMenu, sourseLangMenu } from './components/index.js';
 
 const token = '7984012494:AAGa9jcfdpuxCzcHbcgB6EHw_FdDXVPw5eQ';
 
 const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const inlineKeyboard = homeMenu;
 
-    bot.sendMessage(chatId, 'به ربات ما خوش اومدید!\n لطفا موتور جستجوی مدنظرتون رو انتخاب کنید 🤖', inlineKeyboard);
+    bot.sendMessage(
+        chatId,
+        'به ربات ما خوش اومدید!\n لطفا موتور جستجوی مدنظرتون رو انتخاب کنید 🤖',
+        homeMenu
+    );
 });
 
 bot.on('callback_query', (query) => {
     const chatId = query.from.id;
     const command = query.data;
+    const messageId = query.message.message_id;
 
+    if (command == 'google') {
+        redis.set(`user:${chatId}:action`, command);
+        bot.editMessageText('ممنون!\n حالا زبان مبدا ترجمه رو انتخاب کنید', {
+            chat_id: chatId,
+            message_id: messageId,
+            reply_markup: sourseLangMenu.reply_markup,
+        });
+    }
+    if (command == 'fa') {
+        redis.set(`user:${chatId}:lang`, command);
+        bot.sendMessage(chatId, 'okkkkk');
+    }
 });
